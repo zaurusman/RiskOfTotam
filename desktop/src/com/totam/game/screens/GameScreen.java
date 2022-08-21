1505750ce88865d1f58c89b0cfec0c35a4a91a7d
Antony <slavin216@gmail.com>
2022-08-21 19:11:32 +0300

    added a basic level (copied from video for debugging puroses)

--- desktop/src/com/totam/game/screens/GameScreen.java
+++ desktop/src/com/totam/game/screens/GameScreen.java
@@ -1,10 +1,12 @@
 package com.totam.game.screens;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
@@ -21,8 +23,11 @@ public class GameScreen implements Screen {
     final RiskOfTotam game;
     private OrthographicCamera camera;
     private Viewport port;
-
     private Hud hud;
+
+    private TmxMapLoader mapLoader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
     Texture bg;
     Texture tile_image;
     Array<Rectangle> tiles;
@@ -32,15 +37,25 @@ public class GameScreen implements Screen {
     public GameScreen(final RiskOfTotam game) {
 
         this.game = game;
+
         bg = new Texture("dark_bg.jpg");
         tile_image = new Texture("tile.png");
+
         hero = new Hero(game);
+
         camera = new OrthographicCamera();
+
         port = new StretchViewport(RiskOfTotam.V_WIDTH, RiskOfTotam.V_HEIGHT, camera);
+
         hud = new Hud(game.batch);
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load("default_level.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+        camera.position.set(port.getWorldWidth() / 2, port.getWorldHeight() / 2, 0);
+        /*
         camera.setToOrtho(false, game.SCREEN_WIDTH, game.SCREEN_HEIGHT);
         tiles = new Array<>();
-        SpawnFloor();
+        SpawnFloor();*/
     }
 
     public void SpawnFloor() {
@@ -58,15 +73,30 @@ public class GameScreen implements Screen {
 
     }
 
+    public void handleInput(float dt) {
+        if(Gdx.input.isTouched()) {
+            camera.position.x += 100 * dt;
+        }
+    }
+
+    public void update(float dt) {
+        handleInput(dt);
+        camera.update();
+        renderer.setView(camera);
+    }
+
     @Override
     public void render(float delta) {
+        update(delta);
 
         Gdx.gl.glClearColor(0, 0, 0, 1);
         ScreenUtils.clear(0, 0, 0.2f, 1);
-        //camera.update();
+
+        renderer.render();
+
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
         game.batch.setProjectionMatrix(camera.combined);
-
+        /*
         game.batch.begin();
 
         game.batch.draw(bg, 0, 0);
@@ -102,7 +132,7 @@ public class GameScreen implements Screen {
         }
 
         game.batch.end();
-
+        */
         hud.stage.draw();
     }
 
